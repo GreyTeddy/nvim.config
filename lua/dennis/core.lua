aa9900fa931a3d9e8f4c172affe14d4297050a56
Dennis Duka <dukadennis7@gmail.com>
2023-03-17 08:23:23 +0000

    initial commit

---
+++ lua/dennis/core.lua
@@ -0,0 +1,29 @@
+local opt = vim.opt
+
+-- line numbers
+opt.relativenumber = true
+opt.number = true
+
+-- tabs
+opt.tabstop = 4
+opt.shiftwidth = 4
+opt.expandtab = true
+opt.autoindent = true
+
+-- search
+opt.ignorecase = true
+opt.smartcase = true
+
+-- cursor line
+opt.cursorline = true
+
+-- appearance
+opt.termguicolors = true
+opt.signcolumn = "no"
+
+-- backspace
+opt.backspace = "indent,eol,start"
+
+-- split windows
+opt.splitright = true
+opt.splitbelow = true
